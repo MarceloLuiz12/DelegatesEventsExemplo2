441ec3f1ff2555f3895f31a94e3f4d57094e2427
Marcelo Luiz Pinheiro <marcelo_luiz17@outlook.com>
2023-01-07 10:10:56 -0300

    Add project files.

---
+++ Lista.cs
@@ -0,0 +1,43 @@
+﻿namespace DelegatesEventsExemplo2
+{
+    public delegate void Notificacao();
+    public class Lista
+    {
+        private List<Lista> _items = new();
+        public int Id { get; set; }
+        public string Item { get; set; }
+        public DateTime DataCriacao => DateTime.Now;
+        public IReadOnlyCollection<Lista> BuscarLista => _items;
+        public event Notificacao Notificar;
+
+        public void Add(string item)
+        {
+            var itemLista = new Lista();
+            itemLista.Id += _items.Count;
+            itemLista.Item = item;
+            _items.Add(itemLista);
+            EventHandler();
+        }
+
+        private void EventHandler()
+          => Notificar();
+
+        public override string ToString()
+         => string.Join("-", Id, Item, DataCriacao);
+
+        #region Func
+        public Lista GetItemById(Func<Lista, bool> func)
+            => _items.FirstOrDefault(func);
+        #endregion
+
+        #region Action
+        public void PrintAllItems(Action<Lista> action)
+            => _items.ForEach(action);
+        #endregion
+
+        #region Predicate
+        public bool ExistItem(Predicate<Lista> pred)
+         => _items.Exists(pred);
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using DelegatesEventsExemplo2;
+
+var lista = new Lista();
+
+
+lista.Notificar += Notificar;
+lista.Add("A");
+lista.Add("B");
+lista.Add("C");
+lista.Add("D");
+
+Action<Lista> action = new(i => Console.WriteLine(i));
+Func<Lista, bool> func = new(i => i.Id == 1);
+Predicate<Lista> pred = new(i => i.Id == 999);
+
+//lista.PrintAllItems(action);
+//Console.WriteLine(lista.GetItemById(func));
+Console.WriteLine(lista.ExistItem(pred));
+
+static void Notificar()
+{
+    Console.WriteLine("Fui notificado");
+}
